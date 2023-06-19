9acdd8dbddd83738aa5d182830dac2a4505e55f2
Christina2208 <christinalf22@gmail.com>
2023-06-19 14:20:29 -0400

    included packages and array of questions

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquierer = require('inquierer');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of (objects) questions for user input
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Write a short description on what this porject is about.'
+},
+{
+    type: 'input',
+    name:
+    message:
+},
+{
+    type: 'input',
+    name:
+    message:
+},
+{
+    type: 'input',
+    name:
+    message:
+},
+{
+    type: 'input',
+    name:
+    message:
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
